9469b506b070eadcdbf50aa4c35ec31cdb2fa44b
cavcourt <courtneycavanah@gmail.com>
2025-04-15 21:09:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+(function() {
+    var gc = document.createElement("script");
+    gc.setAttribute("data-goatcounter", "https://chromonospace.goatcounter.com/count");
+    gc.async = true;
+    gc.src = "//gc.zgo.at/count.js";
+    document.body.appendChild(gc);
+  })();
+  
